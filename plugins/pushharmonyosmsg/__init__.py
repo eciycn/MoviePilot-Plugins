1db25daf88be2207a5926fc6063d3baf603761a6
jack wang <yangshunlong3@gmail.com>
2025-06-16 01:16:36 +0800

    V5

--- plugins/pushharmonyosmsg/__init__.py
+++ plugins/pushharmonyosmsg/__init__.py
@@ -15,7 +15,7 @@ class PushHarmonyOsMsg(_PluginBase):
     # 插件图标
     plugin_icon = "Pushplus_A.png"
     # 插件版本
-    plugin_version = "1.0"
+    plugin_version = "1.1"
     # 插件作者
     plugin_author = "eciycn"
     # 作者主页
