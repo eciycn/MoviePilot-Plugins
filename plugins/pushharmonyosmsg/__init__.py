3096dda5b17d29aaee2aae40109ef50a38c8a426
eciycn <49083074+eciycn@users.noreply.github.com>
2025-06-21 21:55:23 +0800

    Up v1.44

--- plugins/pushharmonyosmsg/__init__.py
+++ plugins/pushharmonyosmsg/__init__.py
@@ -19,7 +19,7 @@ class PushHarmonyOsMsg(_PluginBase):
     # 插件图标
     plugin_icon = "Pushplus_A.png"
     # 插件版本
-    plugin_version = "1.43"
+    plugin_version = "1.44"
     # 插件作者
     plugin_author = "eciycn"
     # 作者主页
@@ -209,15 +209,12 @@ class PushHarmonyOsMsg(_PluginBase):
                 logger.info(f"消息类型 {msg_type.value} 未开启消息发送")
                 continue
 
+                logger.info(f"YSL:title: {title}; text: {title} ")
+
             # 尝试发送消息
             try:
-                #sc_url = "https://iyuu.cn/%s.send?%s" % (self._token, urlencode({"text": title, "desp": text}))
-                #sc_url = "http://api.chuckfang.com/%s/%s" % (self._token, urlencode({"text": title, "desp": text}))
-				#sc_url = "http://api.chuckfang.com/%s/%s" % (self._token, urlencode({"text": title, "desp": text}))
-				#sc_url = "http://api.chuckfang.com/%s/%s/%s" % (self._token, urlencode("text": title), urlencode("desp": text))
-                #sc_url = f"http://api.chuckfang.com/{self._token}/{quote(title)}/{quote(text)}"
+                #sc_url = "https://iyuu.cn/%s.send?%s" % (self._token, urlencode({"text": title, "desp": text}))     
                 sc_url = "http://api.chuckfang.com/%s/%s/%s" % (self._token, quote(title), quote(text))
-                #sc_url = "http://api.chuckfang.com/%s/%s/%s" % (self._token, urlencode({"text": title}),urlencode({"desp": text}))
 
                 #logger.info("sc_url %s" % )
                 logger.error(f"sc_url，{str(sc_url)}")
