415702124aa2ea9967e4ef641baa673a6d4885c7
eciycn <49083074+eciycn@users.noreply.github.com>
2025-06-21 18:00:04 +0800

    UP 1.4

--- plugins/pushharmonyosmsg/__init__.py
+++ plugins/pushharmonyosmsg/__init__.py
@@ -19,7 +19,7 @@ class PushHarmonyOsMsg(_PluginBase):
     # 插件图标
     plugin_icon = "Pushplus_A.png"
     # 插件版本
-    plugin_version = "1.3"
+    plugin_version = "1.4"
     # 插件作者
     plugin_author = "eciycn"
     # 作者主页
@@ -213,7 +213,8 @@ class PushHarmonyOsMsg(_PluginBase):
             try:
                 #sc_url = "https://iyuu.cn/%s.send?%s" % (self._token, urlencode({"text": title, "desp": text}))
                 #sc_url = "http://api.chuckfang.com/%s/%s" % (self._token, urlencode({"text": title, "desp": text}))
-                sc_url = "http://api.chuckfang.com/%s/%s/%s" % (self._token, urlencode({"text": title}),urlencode({"desp": text}))
+				sc_url = "http://api.chuckfang.com/%s/%s" % (self._token, urlencode({"text": title, "/desp": text}))
+                #sc_url = "http://api.chuckfang.com/%s/%s/%s" % (self._token, urlencode({"text": title}),urlencode({"desp": text}))
 
                 #logger.info("sc_url %s" % )
                 logger.error(f"sc_url，{str(sc_url)}")
